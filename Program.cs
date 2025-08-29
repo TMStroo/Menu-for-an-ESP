5a3d73214bb09f1fb81549aec1eaea8d465d8c2d
TMStroo <trootroo41@gmail.com>
2025-08-29 10:57:09 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿ using ImGuiNET;
+using ClickableTransparentOverlay;
+using System.Numerics;
+
+namespace GooEyeTesting
+{
+    public class Program : Overlay
+    {
+        bool enableOverlay = false;
+        Vector2 screenSize = new Vector2(1920, 1080);
+        Vector2 drawPosition = new Vector2(150, 150);
+
+
+        // Called every frame
+        protected override void Render()
+        {
+            DrawMenu();
+            DrawOverLay();
+        }
+
+        void DrawMenu()
+        {
+            ImGui.Begin("Main Menu");
+
+            // Checkbox that toggles enableOverlay
+            ImGui.Checkbox("Overlay", ref enableOverlay);
+            ImGui.End();
+        }
+        void DrawOverLay()
+        {
+            if (enableOverlay)
+            {
+                ImGui.SetNextWindowSize(screenSize);
+                ImGui.SetNextWindowPos(new Vector2(0, 0));
+                ImGui.Begin("overlay", ImGuiWindowFlags.NoDecoration
+                  | ImGuiWindowFlags.NoBackground
+                  | ImGuiWindowFlags.NoBringToFrontOnFocus
+                  | ImGuiWindowFlags.NoMove
+                  | ImGuiWindowFlags.NoInputs
+                  | ImGuiWindowFlags.NoCollapse
+                  | ImGuiWindowFlags.NoScrollbar
+                  | ImGuiWindowFlags.NoScrollWithMouse
+                   );
+                ImDrawListPtr drawList = ImGui.GetWindowDrawList();
+                
+                drawList.AddRect(drawPosition,Vector2.Add(drawPosition,new Vector2(300, 150)), ImGui.ColorConvertFloat4ToU32(new Vector4(0, 1, 0, 1)));
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Start().Wait(); // Start the overlay loop
+        }
+    }
+}
